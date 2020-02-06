09223f9d9bddaa6cb3c5b7d7932327d9e41da6ed
Stan <stanislas.kiesgen.de.richter.20170561@efrei.net>
2020-02-06 16:36:20 +0100

    Menu avec fragments vides

--- Appli/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Appli/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrigoFragment frigoFragment;
+    private ListFragment listFragment ;
+    private MenuFragment menuFragment ;
+    private RecetteFragment recetteFragment ;
+    private SettingsFragment settingsFragment ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        frigoFragment = new FrigoFragment();
+        listFragment = new ListFragment();
+        menuFragment = new MenuFragment();
+        recetteFragment = new RecetteFragment();
+        settingsFragment = new SettingsFragment();
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+           case R.id.navigation_Frigo:
+               InitializeFragments(frigoFragment);
+                return true;
+            case R.id.navigation_Liste:
+                InitializeFragments(listFragment);
+                return true;
+            case R.id.navigation_Menu:
+                InitializeFragments(menuFragment);
+                return true;
+            case R.id.navigation_Recette:
+                InitializeFragments(recetteFragment);
+                return true;
+            case R.id.navigation_Settings:
+                InitializeFragments(settingsFragment);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void InitializeFragments(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit(); // save changes
+    }
+
 }
