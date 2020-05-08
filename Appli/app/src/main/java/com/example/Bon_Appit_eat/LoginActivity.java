7658ace995ea7d50fa0c4b388fab41b5c3671f05
Tristan <tristan.bonneau@efrei.net>
2020-05-08 16:15:20 +0200

    nouveau design login

--- Appli/app/src/main/java/com/example/Bon_Appit_eat/LoginActivity.java
+++ Appli/app/src/main/java/com/example/Bon_Appit_eat/LoginActivity.java
@@ -3,10 +3,13 @@ package com.example.Bon_Appit_eat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
 
