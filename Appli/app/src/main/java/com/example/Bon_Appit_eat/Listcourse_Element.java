79dbf2f98992c68eb240ab9cda28baf9b8501ee6
alexis <alexis.lebrun@efrei.net>
2020-05-08 23:10:44 +0200

    test list

---
+++ Appli/app/src/main/java/com/example/Bon_Appit_eat/Listcourse_Element.java
@@ -0,0 +1,17 @@
+package com.example.Bon_Appit_eat;
+
+public class Listcourse_Element {
+        String Ingredient_name;
+        String Qty_needed;
+        int    Ingredient_img;
+        int    CartQuantity;
+
+        public Listcourse_Element(String Ingredient_name, String Qty_needed, int Ingredient_img, int CartQantity) {
+            Ingredient_name = Ingredient_name;
+            Qty_needed = Qty_needed;
+            Ingredient_img = Ingredient_img;
+            CartQantity = CartQantity;
+        }
+
+
+}
--- Appli/app/src/main/java/com/example/Bon_Appit_eat/MainActivity.java
+++ Appli/app/src/main/java/com/example/Bon_Appit_eat/MainActivity.java
@@ -14,10 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.auth.FirebaseAuth;
-
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements DialogueElement.DialogueElementListener {
 
@@ -101,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements DialogueElement.D
                 InitializeFragments(frigoFragment);
                 return true;
             case R.id.navigation_Liste:
+                //
                 InitializeFragments(listFragment);
                 return true;
             case R.id.navigation_Menu:
